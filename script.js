0c0cc1b87f86f8ccdd59b50d38e9336e90a5b000
imbranatoInformatico <dariomantovani@Mini-di-dario-2.homenet.telecomitalia.it>
2022-11-14 16:15:52 +0100

    finish with bug..

--- script.js
+++ script.js
@@ -4,42 +4,44 @@
 let billInput = document.getElementById('billInput');
 let tips = document.querySelectorAll('.btn_tip');
 let numberPeople = document.getElementById('inputPeople');
+let resetBtn = document.querySelector('.btn_reset');
 let numberBill;
 
 
 billInput.addEventListener('input', function(e){
     numberBill = e.target.value;
-    console.log(numberBill);
 
     numberPeople.addEventListener('input', function(e){
-        let peopleNumber = e.target.value;
-        console.log(peopleNumber);
-   
+        let peopleNumber = e.target.value;   
 
-    tips.forEach(function(tip){
-       tip.addEventListener('click', function(){
-            console.log(tip.value);
-            let tipAmount = (numberBill * tip.value * peopleNumber) / 100;
-            console.log(tipAmount);
-            document.getElementById('textTipAmount').innerHTML = tipAmount;
+        tips.forEach(function(tip){
+        tip.addEventListener('click', function(){
+
+                
+
+                let tipAmount = (numberBill * tip.value / 100) / peopleNumber;
+                console.log('person tip :' + tipAmount);
+
+                let singleAmount = numberBill / peopleNumber;
+                console.log('single amount :'+ singleAmount);
+
+                document.getElementById('textTipAmount').innerHTML = tipAmount.toFixed(2);
+                document.getElementById('singleAmount').innerHTML = singleAmount.toFixed(2);
+              
+            })
+
+            
         })
-
-        
-    })
-});
+    });
  
 });
 
+resetBtn.addEventListener('click', function(){
+
+    document.getElementById('textTipAmount').innerHTML = '$0.00';
+    document.getElementById('singleAmount').innerHTML = '$0.00';
+
+});
 
 
-/*  function tipBill (tips){
-    tips.forEach(function(tip){
-        tip.addEventListener('click', function (e) {
-          //console.log(tip.value);
-          let tipSelect = e.target.value;
-          console.log(tipSelect);      
 
-        });
-    });  
-    
-}  */
